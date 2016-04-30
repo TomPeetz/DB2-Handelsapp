5d86236606d340726dd4bc2071b9c00657be4a7d
TomPeetz <TomPeetz@gmx.de>
2016-04-30 18:55:23 +0200

    Projekt erstellt

---
+++ app/src/main/java/db2/db2_handelsapp/MainActivity.java
@@ -0,0 +1,13 @@
+package db2.db2_handelsapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
