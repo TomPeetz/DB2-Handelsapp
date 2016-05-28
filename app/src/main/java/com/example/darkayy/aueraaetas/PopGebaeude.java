f6b372bdaf2e06fb0bf0cdcb99519b0c89449af4
TomPeetz <TomPeetz@gmx.de>
2016-05-28 13:57:18 +0200

    lager.java zu Lager.java umbenant PopGebaeude ButtonListener hinzugefügt und auf Funktion umgeleitet

--- app/src/main/java/com/example/darkayy/aueraaetas/PopGebaeude.java
+++ app/src/main/java/com/example/darkayy/aueraaetas/PopGebaeude.java
@@ -4,8 +4,12 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import com.example.darkayy.aueraaetas.util.Playerdata;
+import com.example.darkayy.aueraaetas.webapi.API_Connection;
 
 import java.util.ArrayList;
 
@@ -14,6 +18,7 @@ import java.util.ArrayList;
  */
 public class PopGebaeude extends Activity {
 
+    Button buy;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,7 +35,7 @@ public class PopGebaeude extends Activity {
         getWindow().setLayout((int) (width * .8), (int) (height * .8));
 
         Intent i = getIntent();
-        String gebaeudename = i.getStringExtra("Gebäudename");
+        final String gebaeudename = i.getStringExtra("Gebäudename");
         String gebaeudeinfo = i.getStringExtra("Gebaeudeinfos");
 
 
@@ -38,6 +43,17 @@ public class PopGebaeude extends Activity {
         name.setText(gebaeudename);
         TextView beschr = (TextView)findViewById(R.id.txtBeschreibung);
         beschr.setText(gebaeudeinfo);
+        buy = (Button)findViewById(R.id.btnGebaeudekaufen);
+        buy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                buyGebaeude(gebaeudename);
+            }
+        });
+    }
 
+    protected void buyGebaeude(String name) {
+        API_Connection con = new API_Connection();
+        con.query(API_Connection.HIGHERGEBLVL, new String[]{Integer.toString(Playerdata.getId()), name});
     }
 }
